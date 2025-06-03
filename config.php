419802dbfc3ae5059f80c1719b801574d75dfbe1
Orbunde-B-A <orbundebenjaminaondofa@gmail.com>
2025-06-03 02:55:05 +0100

    modified:   config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+    define('DB_SERVER', 'localhost');
+    define('DB_USERNAME', 'dev_user');
+    define('DB_PASSWORD', 'Benjamin4$');
+    define('DB_NAME', 'task_db');
+
+
+    //db connection
+    $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+    //CHECK CONNECTION
+    if($conn === false){
+        die("Error:". $mysqli_connect_error);
+    }
+?>
